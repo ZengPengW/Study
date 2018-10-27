1b2c9854a3182870f2b490055dd59753a83fde78
ZengPengW <44264584+ZengPengW@users.noreply.github.com>
2018-10-27 12:02:21 +0800

    day9

--- 算法练习/铺瓷砖/PuCiZhuang.java
+++ 算法练习/铺瓷砖/PuCiZhuang.java
@@ -1,5 +1,3 @@
-
-
 import java.util.Scanner;
 
 public class PuCiZhuang {
@@ -7,7 +5,7 @@ public class PuCiZhuang {
 	private static int n;
 	private static int m;
 	private static final int MOD = 65521;
-	
+	private static boolean[] bl;
 	private static long sum = 0;
 	private static long summ = 0;
 	private static int[][] style = {
@@ -23,19 +21,33 @@ public class PuCiZhuang {
 		n = sc.nextInt();
 		m = sc.nextInt();
 		int[][] book = new int[n + 2][m + 2];
-		dfs(book, 1, 1);
+		bl = new boolean[n + 2];
+		dfs(book, 1, 1, 2);
 		System.out.println(summ % MOD);
-		
+
 	}
 
-	public static void dfs(int[][] book, int id1, int id2) {
+	public static void dfs(int[][] book, int id1, int id2, int id3) {
 
+		if (id1 >= 4 && bl[id1] == false) {
+			int check = 0;
+			for (int i = id1 - 3; i <= id1 - 2; i++) {
+				for (int j = 1; j <= m; j++) {
+					check = check + book[i][j];
+				}
+			}
+			if (check != 2 * m) {
+				return;
+			} else {
+				bl[id1] = true;
+			}
+		}
 		if (sum == n * m) {
 			summ = (summ + 1) % MOD;
 			return;
 		}
 
-		for (int j = id1; j <= n; j++) {
+		for (int j = id1; j <= id3; j++) {
 			for (int k = id2; k <= m; k++) {
 
 				for (int i = 0; i < 8; i++) {
@@ -61,9 +73,10 @@ public class PuCiZhuang {
 						sum = sum + style[i][0] + style[i][1] + style[i][2]
 								+ style[i][3] + 1;
 
-						dfs(book, j, k + 1);
+						dfs(book, j, k + 1, n);
 
-						sum = sum- (style[i][0] + style[i][1] + style[i][2]
+						sum = sum
+								- (style[i][0] + style[i][1] + style[i][2]
 										+ style[i][3] + 1);
 
 						book[j][k] = 0;
