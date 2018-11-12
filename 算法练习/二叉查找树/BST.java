41131ca19b67697c078179ed531a9982ebac9b75
ZengPengW <44264584+ZengPengW@users.noreply.github.com>
2018-11-12 14:10:53 +0800

    add

--- 算法练习/二叉查找树/BST.java
+++ 算法练习/二叉查找树/BST.java
@@ -3,16 +3,22 @@ public class BST {
 	public static void main(String[] args) {
 
 		BST bs=new BST();
-		//Node n1=bs.new Node(1, "a", 1);
-		bs.put(1, "a");
+		
+		
 		bs.put(3, "c");
 		bs.put(2, "b");
 		bs.put(5, "d");
+		bs.put(11, "a");
+		bs.put(4, "d");
+		bs.put(9, "d");
+		bs.put(8, "a");
 		
 		System.out.println(bs.size());
 		System.out.println(bs.get(2));
-		
-			System.out.print(bs.root);
+		System.out.println(bs.min());
+		System.out.println(bs.floor(6));
+		System.out.println(bs.select(3));
+		System.out.print(bs.root);
 		
 		
 	}
@@ -84,6 +90,47 @@ public class BST {
 			x.val = val;
 		x.N = size(x.left) + size(x.right) + 1;
 		return x;
+	}
+	
+	public int min(){
+		return min(root).key;
+	}
+
+	private Node min(Node x) {
+		if (x.left==null) return x;
+		return min(x.left);
+		
+	}
+	
+	public int floor(int key){
+		Node x=floor(root,key);
+		if(x==null)return -1;
+		return x.key;
+		
+	}
+
+	private Node floor(Node x, int key) {
+		if(x==null)return null;
+		int cmp=key>(x.key)?1:key==(x.key)?0:-1;
+		if (cmp<0)return floor(x.left, key);
+		if (cmp==0)return x;
+		Node t=floor(x.right, key);
+		if(t!=null) return t;
+		else return x;
+		
+	
+	}
+	public int select(int k){
+		return select(root,k).key;
+	}
+
+	private Node select(Node x, int k) {
+		
+		if(x==null)return null;
+		int t=size(x.left);
+		if(t>k)return select(x.left, k);
+		else if(t<k) return select(x.right, k-t-1);
+		else return x;
 	}
 
 }
