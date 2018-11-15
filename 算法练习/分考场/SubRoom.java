bd5ba61b231b5c24741bc03c375e7b20e1b21810
ZengPengW <44264584+ZengPengW@users.noreply.github.com>
2018-11-15 16:12:34 +0800

    day29

--- 算法练习/分考场/SubRoom.java
+++ 算法练习/分考场/SubRoom.java
@@ -3,55 +3,67 @@ import java.util.Scanner;
 
 public class SubRoom{
 
-	private static int sum=0;
+	private static int [][]nums;
+	private static int n;
+	private static int m;
+	private static ArrayList<LinkedList<Integer>> room=new ArrayList<LinkedList<Integer>>();
+	private static int min=Integer.MAX_VALUE;
 	public static void main(String[] args) {
-
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int m=sc.nextInt();
-		int[][] stu = new int[m][2];
-		for (int i = 0; i <m; i++) {
-			stu[i][0]=sc.nextInt();
-			stu[i][1]=sc.nextInt();	
+	Scanner scan=new Scanner(System.in);
+	 n=scan.nextInt();
+	 m=scan.nextInt();
+	nums=new int[n+1][n+1];
+	int a,b;
+	for (int i = 0; i < m; i++) {
+		a=scan.nextInt();
+		b=scan.nextInt();
+		nums[a][b]=1;
+		nums[b][a]=1;
+	}
+	dfs(1);
+	System.out.println(min);
+	}
+	public static void dfs(int cur) {
+		
+		if (cur>n) {
+			min=Math.min(room.size(), min);
+			return;
 		}
-		
-		
-		ArrayList<String> al = new ArrayList<String>();
-		
-		ArrayList<String> a2 = new ArrayList<String>();
-		
-		for (int i = 1; i <=n; i++) {
-			al.add(""+i);
-			a2.add(""+i);
+			if (room.size()>=min) {
+				return;
+			}
+		for (int i = 0; i < room.size(); i++) {
+			if (check(i, cur)) {
+				room.get(i).add(cur);
+				dfs(cur+1);
+				room.get(i).remove(room.get(i).size()-1);
+			}
 			
 		}
-
-		for (int j = 1; j <= n; j++) {
-			if (a2.size()<=0) {
-				break;
-			}
-			if (al.contains(""+j)) {
+			
 				
-				for (int i = j-1; i < stu.length; i++) {
-					if (stu[i][0] == j) {
-						
-						al.remove(""+stu[i][1]);
-					}
-
-				}
-				
-					a2.removeAll(al);
-				
-				al.clear();
-				
-					al.addAll(a2);
-				
-				sum++;
-			}
-
+		LinkedList<Integer> rm=new LinkedList<Integer>();
+		rm.add(cur);
+		room.add(rm);
+		dfs(cur+1);
+		room.remove(room.size()-1);	
+			
+		
+			
 		}
-
-		System.out.println(sum);
+		
+	public static boolean check(int id,int cur) {
+		for (int i = 0; i < room.get(id).size(); i++) {
+			if (nums[cur][room.get(id).get(i)]==1) {
+				return false;
+			}
+		}
+		return true;
+				
+		
 	}
+
+
+
 
 }
