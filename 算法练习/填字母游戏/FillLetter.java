e2ae90e9cd6afbdb90275fa11f0eac9a34b2be3b
ZengPengW <44264584+ZengPengW@users.noreply.github.com>
2018-11-15 18:27:07 +0800

    day29

--- 算法练习/填字母游戏/FillLetter.java
+++ 算法练习/填字母游戏/FillLetter.java
@@ -1,122 +1,75 @@
 import java.util.Scanner;
 
-
 public class FillLetter {
 
 	private static String[] game;
+
 	public static void main(String[] args) {
-		Scanner scan=new Scanner(System.in);
-		int n=scan.nextInt();
-		game=new String[n];
-		for (int i = 0; i <n; i++) {
-			game[i]=scan.next();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		game = new String[n];
+		for (int i = 0; i < n; i++) {
+			game[i] = scan.next();
 		}
 		f();
 		scan.close();
 	}
-	
-	public static void f() {
-		for (int i = 0; i < game.length; i++) {
-			char[] ch=game[i].toCharArray();
-			if (one(ch, 0, ch.length)==0) {
-			System.out.println(method(ch, 0, ch.length));	
-			}else System.out.println(1);
-				
+
+	public static boolean check(String str) {
+		if (str.indexOf("*OL") != -1 || str.indexOf("LO*") != -1
+				|| str.indexOf("L*L") != -1) {
+			return true;
 		}
-	}
-	
-	public static int one(char[] ch,int index ,int len) {
-		int flag=0;
-		for (int i = 0; i < len; i++) {
-		   if (ch[i]=='*') {
-			if ((i+1<len&&i>0&&ch[i+1]=='L'&&ch[i-1]=='L')
-				||(i>1&&ch[i-2]=='L'&&ch[i-1]=='O')
-				||(i+2<len&&ch[i+2]=='L'&&ch[i+1]=='O')	
-					) {
-				flag=1;
-				return flag;
-			}	   
-		}		
-	}	
-		return flag;
-}
-	public static int method(char[] ch,int index ,int len) {
-		int flag=0;
-		while (len>index) {
-			
-			while (true) {
-				if (index<len&&ch[index]!='*') 
-					index++;	
-				else break;
-				}
-			if (index>=len) {
-				break;
-			}						
-			if (len>index&&ch[index]=='*') {
-				if((index>1&&ch[index-2]=='L'&&ch[index-1]=='O')){
-					ch[index]='L';
-					flag=1;
-					index++;
-				
-				}else if (index+2<=len-1&&ch[index+2]=='L'&&ch[index+1]=='O') {
-					ch[index]='L';
-					flag=1;
-					index++;
-					
-				}else if (index>0&&index<len-1&&ch[index-1]=='L'&&ch[index+1]=='L') {
-					ch[index]='O';
-					flag=1;
-					index++;
-				}else if(index+1<len&&ch[index+1]=='O') {
-					ch[index]='O';
-					flag=0;
-					index++;
-				}else {
-					ch[index]='L';
-					flag=0;
-					index++;
-				}	
-			}
-		if (flag==1) {
-			break;
-		}
-		
-		while (true) {
-			if (index<len&&ch[index]!='*') 
-				index++;	
-			else break;
-			}
-		if (index>=len) {
-			break;
-		}
-		if (len>index&&ch[index]=='*') {
-			if((index>1&&ch[index-2]=='L'&&ch[index-1]=='O')){
-				ch[index]='L';
-				flag=-1;
-				index++;
-			
-			}else if (index+2<=len-1&&ch[index+2]=='L'&&ch[index+1]=='O') {
-				ch[index]='L';
-				flag=-1;
-				index++;
-				
-			}else if (index>0&&index<len-1&&ch[index-1]=='L'&&ch[index+1]=='L') {
-				ch[index]='O';
-				flag=-1;
-				index++;
-			}else if(index+1<len&&ch[index+1]=='O') {
-				ch[index]='O';
-				flag=0;
-				index++;
-			}else {
-				ch[index]='L';
-				flag=0;
-				index++;
-			}	
-		}
-			
-	}
-		return flag;
+		return false;
 	}
 
+	public static void f() {
+		for (int i = 0; i < game.length; i++) {
+
+			if (!check(game[i])) {
+				System.out.println(method(game[i]));
+			} else
+				System.out.println(1);
+
+		}
+	}
+
+	public static int method(String s) {
+		int flag = 0;
+		while (s.indexOf("*") != -1) {
+			if (check(s)) {
+				if (flag == 0)
+					return 1;
+				else
+					return -1;
+			}
+
+			String newstr = null;
+			int len = -1;
+			while (true) {
+				len = s.indexOf("*", len + 1);
+				if (len == -1) {
+					s = newstr;
+					flag = 1 - flag;
+					break;
+				}
+
+				newstr = s.substring(0, len) + "L"
+						+ s.substring(len + 1, s.length());
+				if (check(newstr))
+					newstr = s.substring(0, len) + "O"
+							+ s.substring(len + 1, s.length());
+
+				if (!check(newstr)) {
+					flag = 1 - flag;
+					s = newstr;
+					break;
+				}
+
+			}
+
+		}
+		return 0;
+
+	}
 }
