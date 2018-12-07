708dedbeaca9584d72e2b3401f3ca3356bb31c48
ZengPengW <44264584+ZengPengW@users.noreply.github.com>
2018-12-07 22:10:28 +0800

    day49

--- 算法练习/爬楼梯/UDStairs.java
+++ 算法练习/爬楼梯/UDStairs.java
@@ -1,53 +1,39 @@
-import java.util.HashMap;
 import java.util.Scanner;
 
-public class Louti {
+public class UDStairs{
 
-	private static HashMap<Long, Long> hs = new HashMap<Long, Long>();
-    private static long m;
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		long n = sc.nextLong();
-		m = sc.nextLong();
-
-		long sum = 0;
-
-		long sum1 = 0;
-		for (long i = 1; i <= n; i++) {
-			sum = jisaun(i);
-			sum1 = (sum1 + sum*sum)%m;
+		Scanner scan=new Scanner(System.in);
+		long n=scan.nextLong();
+		int m=scan.nextInt();
+		
+		
+		
+		if(n<=0){
+			System.out.println(0);
+			return;
 		}
-
-		System.out.println((sum1 + 1) % m);
-	}
-
-	public static long jisaun(long n) {
-
-		if (n <= 0) {
-			return 0;
+		if(n==1){
+			System.out.println(1%m);
+			return;
 		}
-		if (n == 2) {
-			return 2;
+		if(n==2){
+			System.out.println(6%m);
+			return;
 		}
-		if (n == 1) {
-			return 1;
-		}
-		if (hs.containsKey(n)) {
-			return hs.get(n);
-		}
-		long a=1;
-		long b=2;
-        long temp=0;
-        for (int i = 3; i <=n; i++) {
-			temp=(a+b)%m;
+		int a=1;
+		int b=2;
+		int sum=a*a+b*b;
+		for (long i = 3; i <=n; i++) {
+			int temp=(a+b)%m;
 			a=b;
 			b=temp;
-			
+			sum=(sum+temp*temp)%m;
 		}
-			if (!hs.containsKey(n)) {
-			hs.put(n, temp);
-		}
-		return temp;
+		System.out.println((sum+1)%m);
+		
 	}
+//1 2 3 5
+//1 4 9 25
+	
 }
