cc8e22f2f33dffba9f412497895f6fab9565b508
ZengPengW <44264584+ZengPengW@users.noreply.github.com>
2019-02-28 16:26:06 +0800

    change

--- 算法练习/穿越雷区/Through.java
+++ 算法练习/穿越雷区/Through.java
@@ -1,99 +1,74 @@
-import java.util.ArrayList;
+
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-public class Through {
-	private static int n;
-	private static int[][] map;
-	private static int[][] sta = new int[1][2];
-	private static int[][] end = new int[1][2];
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		map = new int[n][n];
-		String str;
-		for (int i = 0; i < n; i++) {
+public class Through
+{
+	public static void main(String[] args)
+	{		
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int x = 0,y = 0;
+		String [][]map=new String[n][n];
+		for (int i = 0; i < n; i++) 
 			for (int j = 0; j < n; j++) {
-				str = sc.next();
-				if (str.equals("+")) {
-					map[i][j] = 1;
-				} else if (str.equals("-")) {
-					map[i][j] = 0;
-				} else if (str.equals("A")) {
-					map[i][j] = 2;
-					sta[0][0] = i;
-					sta[0][1] = j;
-				} else {
-					end[0][0] = i;
-					end[0][1] = j;
-					map[i][j] = 3;
+				map[i][j]=sc.next();
+				if(map[i][j].equals("A")) {
+					x=i;
+					y=j;
 				}
 			}
-
+		System.out.println(bfs(map, x, y));
+		
+	}
+	private static class Point{
+		int x,y,bs;
+		String s;
+		public Point(int x,int y,int bs,String s) {
+			this.x=x;
+			this.y=y;
+			this.bs=bs;
+			this.s=s;
 		}
-		int[][] bs = new int[n][n];
-		bfs(map, bs);
-
 	}
-
-	public static int[][] Node(int x, int y) {
-		int[][] arr = new int[1][2];
-		arr[0][0] = x;
-		arr[0][1] = y;
-		return arr;
-	}
-
-	public static void bfs(int[][] map, int[][] bs) {
-
-		ArrayList<int[][]> al = new ArrayList<int[][]>();
-		al.add(Node(sta[0][0], sta[0][1]));
-
-		while (!al.isEmpty()) {
-			int[][] arr = al.remove(0);
-			int ud = arr[0][0];
-			int lr = arr[0][1];
-
-			if (ud - 1 >= 0 && bs[ud - 1][lr] == 0
-					&& map[ud - 1][lr] != map[ud][lr]) {
-				bs[ud - 1][lr] = bs[ud][lr] + 1;
-				al.add(Node(ud - 1, lr));
-				if (ud - 1 == end[0][0] && lr == end[0][1]) {
-					System.out.println(bs[ud - 1][lr]);
-					return;
-				}
+	public static int bfs(String [][]map,int x,int y) {
+		Queue<Point> que=new LinkedList<Point>();
+		
+		que.offer(new Point(x, y,0,"A"));
+		Point p=null;
+		int n=map.length;
+		int [][]mark=new int[n][n];
+		while(!que.isEmpty()) {
+			p=que.poll();
+			//ио
+			if(p.x-1>=0&&mark[p.x-1][p.y]==0&&(!p.s.equals(map[p.x-1][p.y]))) {
+				if(map[p.x-1][p.y].equals("B"))return p.bs+1;
+				mark[p.x-1][p.y]=1;
+				que.add(new Point(p.x-1,p.y,p.bs+1,map[p.x-1][p.y]));
 			}
-			if (ud + 1 < n && bs[ud + 1][lr] == 0
-					&& map[ud + 1][lr] != map[ud][lr]) {
-				bs[ud + 1][lr] = bs[ud][lr] + 1;
-				al.add(Node(ud + 1, lr));
-				if (ud + 1 == end[0][0] && lr == end[0][1]) {
-					System.out.println(bs[ud + 1][lr]);
-					return;
-				}
+			//об
+			if(p.x+1<n&&mark[p.x+1][p.y]==0&&(!p.s.equals(map[p.x+1][p.y]))) {
+				if(map[p.x+1][p.y].equals("B"))return p.bs+1;
+				mark[p.x+1][p.y]=1;
+				que.add(new Point(p.x+1,p.y,p.bs+1,map[p.x+1][p.y]));
 			}
-			if (lr - 1 >= 0 && bs[ud][lr - 1] == 0
-					&& map[ud][lr - 1] != map[ud][lr]) {
-				bs[ud][lr - 1] = bs[ud][lr] + 1;
-				al.add(Node(ud, lr - 1));
-				if (ud == end[0][0] && lr - 1 == end[0][1]) {
-					System.out.println(bs[ud][lr - 1]);
-					return;
-				}
-
+			//вС
+			if(p.y-1>=0&&mark[p.x][p.y-1]==0&&(!p.s.equals(map[p.x][p.y-1]))) {
+				if(map[p.x][p.y-1].equals("B"))return p.bs+1;
+				mark[p.x][p.y-1]=1;
+				que.add(new Point(p.x,p.y-1,p.bs+1,map[p.x][p.y-1]));
 			}
-			if (lr + 1 < n && bs[ud][lr + 1] == 0
-					&& map[ud][lr + 1] != map[ud][lr]) {
-				bs[ud][lr + 1] = bs[ud][lr] + 1;
-				al.add(Node(ud, lr + 1));
-				if (ud == end[0][0] && lr + 1 == end[0][1]) {
-					System.out.println(bs[ud][lr + 1]);
-					return;
-				}
-
+			//ср
+			if(p.y+1<n&&mark[p.x][p.y+1]==0&&(!p.s.equals(map[p.x][p.y+1]))) {
+				if(map[p.x][p.y+1].equals("B"))return p.bs+1;
+				mark[p.x][p.y+1]=1;
+				que.add(new Point(p.x,p.y+1,p.bs+1,map[p.x][p.y+1]));
 			}
-
+			
 		}
-
+		return -1;
+		
 	}
-
+	
 }
