b06db8750ef9243540bdaa283dc54c7bdf4ba7f2
ZengPengW <44264584+ZengPengW@users.noreply.github.com>
2019-02-28 08:10:18 +0800

    add

--- 算法练习/数独/Sudoku.java
+++ 算法练习/数独/Sudoku.java
@@ -1,78 +1,73 @@
 import java.util.Scanner;
 
 public class Sudoku {
-	private static int index = 0;
 
 	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		String str;
+		int [][]num=new int [10][10];
+		int [][]markx=new int [10][10];
+		int [][]marky=new int [10][10];
+		int [][][]mark=new int[4][4][10];
+		for (int i = 1; i <=9; i++) {
+			str=sc.next();
+			for (int j = 1; j <=9; j++) {
+				num[i][j]=str.charAt(j-1)-'0';
+				markx[i][num[i][j]]=1;
+				marky[j][num[i][j]]=1;
 
-		Scanner sc = new Scanner(System.in);
-		int[][] book1 = new int[9][10];
-		int[][] book2 = new int[9][10];
-		int[][] num = new int[9][9];
-		int[][] gezi = new int[9][10];
-		String[] str = new String[9];
-		for (int i = 0; i < 9; i++) {
-			str[i] = sc.next();
-		}
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				num[i][j] = str[i].charAt(j) - '0';
-
-				if (num[i][j] != 0) {
-					index++;
-					gezi[(j / 3) + (i / 3) * 3][num[i][j]] = 1;
-					book1[i][num[i][j]] = 1;
-					book2[j][num[i][j]] = 1;
-				}
+				int idx=(int) Math.ceil(i/3.0);
+				int idy=(int) Math.ceil(j/3.0);
+				mark[idx][idy][num[i][j]]=1;
 			}
 		}
-		f(num, book1, book2, gezi, 0, 0);
+		
+		dfs(num,mark, markx,marky, 1, 1);
 	}
 
-	public static void f(int[][] num, int[][] book1, int[][] book2,
-			int[][] gezi, int x, int y) {
-
-		if (index == 81) {
-			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++) {
-					System.out.print(num[i][j]);
-
+	private static void dfs(int [][]num,int [][][]mark,int [][]markx,int [][]marky,int x,int y) {
+		if(x>9) {
+			for (int i = 1; i <=9; i++) {
+			for (int j = 1; j <=9; j++){
+				System.out.print(num[i][j]);
+			}
+			System.out.println();
+			}
+			return;
+		}
+		if(num[x][y]==0) {
+			
+			int idx=(int) Math.ceil(x/3.0);
+			int idy=(int) Math.ceil(y/3.0);
+					
+			for (int j = 1; j <=9; j++) {
+				
+				if(markx[x][j]==0&&marky[y][j]==0&&mark[idx][idy][j]==0) {
+					num[x][y]=j;
+					markx[x][j]=1;
+					marky[y][j]=1;
+					mark[idx][idy][j]=1;
+					if(y+1<=9) {
+						dfs(num,mark, markx,marky, x, y+1);
+					}else if (y+1>9) {
+						dfs(num,mark, markx,marky, x+1, 1);
+					}
+					num[x][y]=0;
+					markx[x][j]=0;
+					marky[y][j]=0;
+					mark[idx][idy][j]=0;
 				}
-				System.out.println();
 			}
-			System.exit(0);
-		}
-		if (y == 9) {
-			x++;
-			y = 0;
-		}
-		for (int i = 1; i <= 9; i++) {
-			if (num[x][y] != 0) {
-				y++;
-				if (y == 9) {
-					x++;
-					y = 0;
-
-				}
-				i = 0;
-				continue;
+		}else {
+			if(y+1<=9) {
+			dfs(num,mark, markx,marky, x, y+1);
+			}else if (y+1>9) {
+				y=1;
+				x=x+1;
+				dfs(num,mark, markx,marky, x, y);
 			}
-			if (num[x][y] == 0 && book1[x][i] == 0 && book2[y][i] == 0
-					&& gezi[(y / 3) + (x / 3) * 3][i] == 0) {
-				num[x][y] = i;
-				book1[x][i] = 1;
-				book2[y][i] = 1;
-				gezi[(y / 3) + (x / 3) * 3][i] = 1;
-				index++;
-				f(num, book1, book2, gezi, x, y + 1);
-				index--;
-				num[x][y] = 0;
-				book1[x][i] = 0;
-				book2[y][i] = 0;
-				gezi[(y / 3) + (x / 3) * 3][i] = 0;
-			}
-
 		}
-
+		
 	}
 }
--- 算法练习/牌型种数/PaiXing.java
+++ 算法练习/牌型种数/PaiXing.java
@@ -1,31 +1,24 @@
 public class PaiXing {
-	private static int sum = 0;
-	private static int index = 0;
-
-	public static void main(String[] args) {
-		dfs(1);
-		System.out.println(sum);
-
-	}
-
-	public static void dfs(int id) {
-
-		if (index == 13) {
-			sum++;
-			return;
-		}
-
-		if (id > 13 || index > 13) {
+	public static void dfs(long a, int ans) {
+		if (ans == 13) {
+			if (a == 0) {
+				count++;
+			}
 			return;
 		}
 
 		for (int i = 0; i <= 4; i++) {
-			index += i;
-			dfs(id + 1);
-			index -= i;
-
+			dfs(a - i, ans + 1);
 		}
+	}
 
+	public static void main(String[] args) {
+		int[] num = new int[14];
+		for (int i = 0; i < num.length; i++) {
+			num[i] = 4;
+		}
+		dfs(13, 0);
+		System.out.println(count);
 	}
 
 }
