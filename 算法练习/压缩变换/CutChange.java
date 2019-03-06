2771dd3ca0c0389b5be2c6e098649a2cb549f956
ZengPengW <44264584+ZengPengW@users.noreply.github.com>
2019-03-06 16:39:17 +0800

    change

--- 算法练习/压缩变换/CutChange.java
+++ 算法练习/压缩变换/CutChange.java
@@ -4,32 +4,32 @@ import java.util.Scanner;
 
 public class CutChange {
 
-	private static int n;
-	private static LinkedList<Long> ls=new LinkedList<Long>();
-	private static long[] num;
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		n=sc.nextInt();
-		num=new long[n];
-		for (int i = 0; i < num.length; i++) 
-			num[i]=sc.nextLong();
-			f();
-
-	}
-	public static void f() {
-		int index;
-		for (int i = 0; i < num.length; i++) {
-			 index=ls.indexOf(num[i]);
-			if (index==-1) {
-				System.out.print((num[i]*-1)+" ");
-				ls.add(num[i]);
-			}else {
-				ls.add(num[i]);
-				System.out.print((ls.size()-index-2)+" ");
-				ls.remove(index);
-			}	
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] num = new int[n];
+		for (int i = 0; i < n; i++) {
+			num[i] = sc.nextInt();
 		}
-		
+		f(num);
+	}
+
+	public static void f(int[] num) {
+
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		for (int i = 0, len = num.length; i < len; i++) {
+			if (!list.contains(num[i])) {
+				System.out.print("-"+num[i] + " ");
+				list.add(num[i]);
+			} else {
+				int index = list.lastIndexOf(num[i]);
+				System.out.print((list.size() - index - 1)+" ");
+				list.remove(index);
+				list.add(num[i]);
+			}
+		}
+	}
+
 		
 		
 		
