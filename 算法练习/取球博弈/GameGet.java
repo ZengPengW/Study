c3fd3599e1957906750132f9ec6615f661771b0d
ZengPengW <44264584+ZengPengW@users.noreply.github.com>
2019-03-06 15:17:52 +0800

    change

--- 算法练习/取球博弈/GameGet.java
+++ 算法练习/取球博弈/GameGet.java
@@ -1,87 +1,88 @@
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class GameGet {
-	private static int[] format = new int[3];
-	private static int[] game = new int[5];
-	private static int[] ps = new int[2];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < format.length; i++) {
-			format[i] = sc.nextInt();	
+		int[] n = new int[3];
+		int[] x = new int[5];
+		for (int i = 0; i < 3; i++) {
+			n[i] = sc.nextInt();
 		}
-		Arrays.sort(format);
-
-		for (int i = 0; i < game.length; i++)
-			game[i] = sc.nextInt();
-		f();
+		for (int i = 0; i < 5; i++) {
+			x[i] = sc.nextInt();
+		}
+		pk(n, x);
 	}
 
-	public static void f() {
+	public static void pk(int[] n, int[] x) {
+		Arrays.sort(n);
+
 		for (int i = 0; i < 5; i++) {
-			int num = game[i];
-			ps[0] = 0;
-			ps[1] = 0;
+			int num = x[i];
+			int p1 = 0;
+			int p2 = 0;
 			while (num > 0) {
-				if (format[0] > num)
-					break;
-				boolean b1 = false;
+				//Íæ¼Ò1
+				boolean bl = false;
 				for (int j = 2; j >= 0; j--) {
-					if ((format[j] % 2 == 0 && ps[0] % 2 == 0)
-							|| (format[j] % 2 != 0 && ps[0] % 2 != 0))
+					if ((n[j] % 2 == 0 && p1 % 2 == 0) || (p1 % 2 != 0 && n[j] % 2 != 0))
 						continue;
-					if (num - format[j] >= 0) {
-						ps[0] += format[j];
-						num -= format[j];
-						b1 = true;
+					if (num - n[j] >= 0) {
+						p1 += n[j];
+						num -= n[j];
+						bl = true;
 						break;
 					}
 				}
 
-				if (!b1) {
+				if (!bl) {
 					for (int j = 2; j >= 0; j--) {
-						if (num - format[j] >= 0) {
-							ps[0] += format[j];
-							num -= format[j];
+						if (num - n[j] >= 0) {
+							p1 += n[j];
+							num -= n[j];
+							bl = true;
 							break;
 						}
 					}
 				}
-
-				boolean b2 = false;
+				if(bl!=true)break;
+				if(num<=0)break;
+				//Íæ¼Ò2
+				bl = false;
 				for (int j = 2; j >= 0; j--) {
-					if ((format[j] % 2 == 0 && ps[1] % 2 == 0)
-							|| (format[j] % 2 != 0 && ps[1] % 2 != 0))
+					if ((n[j] % 2 == 0 && p2 % 2 == 0) || (p2 % 2 != 0 && n[j] % 2 != 0))
 						continue;
-					if (num - format[j] >= 0) {
-						ps[1] += format[j];
-						num -= format[j];
-						b2 = true;
+					if (num - n[j] >= 0) {
+						p2 += n[j];
+						num -= n[j];
+						bl = true;
 						break;
 					}
-
 				}
 
-				if (!b2) {
+				if (!bl) {
 					for (int j = 2; j >= 0; j--) {
-						if (num - format[j] >= 0) {
-							ps[1] += format[j];
-							num -= format[j];
+						if (num - n[j] >= 0) {
+							p2 += n[j];
+							num -= n[j];
+							bl = true;
 							break;
 						}
 					}
 				}
+				if(bl!=true)break;
+				if(num<=0)break;	
 			}
-
-			if ((ps[0] % 2 == 0 && ps[1] % 2 == 0)
-					|| (ps[0] % 2 != 0 && ps[1] % 2 != 0)) {
+			if((p1%2==0&&p2%2==0)||(p1%2==1&&p2%2==1))
 				System.out.print("0 ");
-			} else if (ps[0] % 2 != 0 && ps[1] % 2 == 0) {
+			else if ((p1%2==1&&p2%2==0)) 
 				System.out.print("+ ");
-			} else {
-				System.out.print("- ");
-			}
+			else System.out.print("- ");
+			
 		}
+
 	}
 }
