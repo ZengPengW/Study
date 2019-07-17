12bd3a0a3c39615c8edff46fa82c5f129d23124b
ZengPengW <44264584+ZengPengW@users.noreply.github.com>
2019-07-17 22:51:33 +0800

    springcloud-study

---
+++ 项目练习/SpringCloud-study/microservicecloud/microservicecloud-api/src/main/java/com/zp/springcloud/entities/Dept.java
@@ -0,0 +1,31 @@
+package com.zp.springcloud.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * 部门实体类
+ * 
+ * @author zp
+ *
+ */
+@SuppressWarnings("serial")
+@NoArgsConstructor
+@Data
+@Accessors(chain = true)
+public class Dept implements Serializable {
+
+	private Long deptno;// 主键
+	private String dname;// 部门名称
+	private String dp_source;// 来自哪个数据库，有可能同一个信息存储到不同数据库
+
+	public Dept(String dname) {
+		super();
+		this.dname = dname;
+	}
+
+}
